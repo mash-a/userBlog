449e35d0dfbac79cde99a80a66fa110bbd9d19aa
mash-a <mariya.emv@gmail.com>
2018-06-21 16:12:55 -0400

    configure routes to use knex and pg

--- app.js
+++ app.js
@@ -21,13 +21,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', index);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
-const knex = require('knex')
+const environment = process.env.NODE_ENV || 'development';
+const knexConfig = require('../knexfile.js')[environment];
+const knex = require('knex')(knexConfig);
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  knex('userblog')
+  knex('users')
     .then(function(users){
       res.render('users', {
         users
       })
+    })
+});
+
+router.get('/:id', function(req, res, next) {
+  knex('users')
+    .where('id', req.params.id)
+    .first()
+    .then(function(user){
+      knex('posts')
+        .where('user_id', req.params.id)
+        .then(function(posts){
+          res.render('user', {
+            user,
+            posts
+          })
+        })
     })
 });
 
