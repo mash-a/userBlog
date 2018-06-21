c2092cdfbab5829ace6f3b74e32703a8e465aae0
Kin Siu <kina.siu@gmail.com>
2018-06-21 15:35:39 -0400

    added get all get one routes

--- routes/users.js
+++ routes/users.js
@@ -5,11 +5,27 @@ const knex = require('knex')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  knex('userblog')
+  knex('users')
     .then(function(users){
       res.render('users', {
         users
       })
+    })
+});
+
+router.get('/:id', function(req, res, next) {
+  knex('users')
+    .where('id', req.params.id)
+    .first()
+    .then(function(user){
+      knex('posts')
+        .where('user_id', req.params.id)
+        .then(function(posts){
+          res.render('user', {
+            user,
+            posts
+          })
+        })
     })
 });
 
